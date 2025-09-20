d57ea62a818aa6ec72a0a3e76f12ea624229d7c5
MD MUBAROK <contact.mdmubarok@gmail.com>
2025-09-20 11:20:20 +0000

    এই ইমেজগুলো স্লাইডার ফোল্ডারের ভিতরে রাখুন

--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
-      {
-        protocol: 'https',
-        hostname: 'blogger.googleusercontent.com',
-      },
     ],
   },
 };
